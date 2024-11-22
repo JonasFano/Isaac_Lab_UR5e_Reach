706546fd98b44db9e9891ae5e656487a2e9f4658
Jonas Fano <fanojoni@gmail.com>
2024-11-22 14:43:19 +0100

    Add weights and biases scripts

--- train_sb3_wandb.py
+++ train_sb3_wandb.py
@@ -35,11 +35,11 @@ from wandb.integration.sb3 import WandbCallback
 def main():
     """Train with stable-baselines agent."""
     # WandB initialization (config.yaml values come from WandB during sweep)
-    with open("./config_0_1.yaml") as file:
+    with open("./config.yaml") as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
 
     run = wandb.init(
-        project="rel_ik_sb3_ppo_ur5e_cube_lift_0_1_v3",
+        project="rel_ik_sb3_ppo_ur5e_reach",
         config=config,
         sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
         monitor_gym=False,  # auto-upload the videos of agents playing the game
@@ -47,8 +47,8 @@ def main():
     )
 
     # Load env cfg
-    task = "UR5e-Lift-Cube-IK" # "UR5e-Lift-Cube"
-    num_envs = 4096
+    task = "UR5e-Reach-IK" # "UR5e-Lift-Cube"
+    num_envs = 8192
     device = "cuda"
     env_cfg = parse_env_cfg(task, device=device, num_envs=num_envs)
     env_cfg.seed = wandb.config["seed"]
