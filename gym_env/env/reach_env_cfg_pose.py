4b51e7c3e5036d5d4b86765cdce8f87947f1b905
Jonas Fano <jofa@universal-robots.com>
2025-02-09 09:56:57 +0100

    Change robot reset randomization

--- gym_env/env/reach_env_cfg_pose.py
+++ gym_env/env/reach_env_cfg_pose.py
@@ -288,7 +288,7 @@ class EventCfg:
         func=mdp.reset_joints_by_scale,
         mode="reset",
         params={
-            "position_range": (0.8, 1.2),
+            "position_range": (0.7, 1.3),
             "velocity_range": (0.0, 0.0),
         },
     )
